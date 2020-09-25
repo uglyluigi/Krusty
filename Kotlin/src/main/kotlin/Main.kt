c06cac1f6e1270af5b3689f0304aea237f24e618
uglyluigi <5006946+uglyluigi@users.noreply.github.com>
2020-09-24 22:55:08 -0500

    Add Discord4J and dotenv dependencies; bot can now login

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -1,12 +1,30 @@
 package bot
 
 import bindings.RustDefs
+import discord4j.core.DiscordClient
+import discord4j.core.`object`.entity.Message
+import discord4j.core.event.domain.message.MessageCreateEvent
+import io.github.cdimascio.dotenv.dotenv
 
 open class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            RustDefs.exampleMethod()
+            val dotenv = dotenv()
+            val client = DiscordClient.create(dotenv["BOT_TOKEN"])
+            val gateway = client.login().block()
+
+            gateway?.on(MessageCreateEvent::class.java)?.subscribe {
+                event: MessageCreateEvent ->
+                val message = event.message
+
+                if ("!ping" == event.message.content) {
+                    val channel = message.channel.block()
+                    channel?.createMessage("Pong!")?.block()
+                }
+            }
+
+            gateway?.onDisconnect()?.block()
         }
     }
 }
